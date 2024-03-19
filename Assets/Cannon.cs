5d9bb43e7429f8bc1dc0cb794a6769c5b63a9caa
Isaac Pontier <144137723+Isaac-Pontier-Student@users.noreply.github.com>
2024-03-18 23:10:34 -0400

    Created cannon script and object to shoot projectiles

---
+++ Assets/Cannon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform fireSocket;
+    public float rotationRate = 90.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float aimInput = Input.GetAxis("Horizontal");
+        aimInput *= rotationRate * Time.deltaTime;
+        transform.Rotate(Vector3.right * aimInput, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(projectilePrefab, fireSocket.position, fireSocket.rotation);
+    }
+}
--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -17,10 +17,24 @@ public class Projectile : MonoBehaviour
 
         rigidbody.AddForce(forceDirection * forceAmount, ForceMode.VelocityChange);
     }
+    //messages are methods in a script that are only called/used if they are defined. EG Update or OnCollisionEnter won't be used if we don't call them
+    //start and update are messages sent from unity engine to our scripts too. These are part of the MonoBehavior Lifecycle
+    void OnCollisionEnter(Collision other) //OnCollisionEnter is a built-in MonoBehavior method but it only does anything when the scripts define it in their own functions (here)
 
+    {
+        print("Collides with " + other.gameObject.name);
+        //"other" is our instance of the Collision class
+        //"gameObject" is the game object that we are colliding with
+        if (other.gameObject.CompareTag("Castle"))
+        {
+            Destroy(other.gameObject);
+            //so "other" (especially when followed by .gameObject?) refers to the other object that the item that this script is attached to is colliding with. It's the other guy.
+        }
+    
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
