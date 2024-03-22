6ee574654ce995b69e0e93cde29d222405c479a5
Isaac Pontier <dapoe56@student.oaisd.org>
2024-03-22 09:55:40 -0400

    Terrain package import, made lvl1 terrain

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -19,24 +19,19 @@ public class Score : MonoBehaviour
         if (cannon)
         {
             int numProjectiles = cannon.numProjectiles;
-            //use this to determine how well the player did to determine score at end of level
-            //Star rating depends on number of projectiles fired
-            if (numProjectiles == threeStarsValue)
+            if (numProjectiles <= threeStarsValue)
             { 
-                print("3 stars");
                 scoreDisplay.text = "Three Stars!";
                 scoreAnimator.SetInteger("Stars", 3);
             }
             else if (numProjectiles == twoStarsValue) 
             { 
-                print("2 stars");
                 scoreDisplay.text = "Two Stars.";
                 scoreAnimator.SetInteger("Stars", 2);
             }
             else 
             { 
-                print("1 star");
-                scoreDisplay.text = "One Star :(";
+                scoreDisplay.text = "One Star...";
                 scoreAnimator.SetInteger("Stars", 1);
             }
             Invoke("NextLevel", 2);
